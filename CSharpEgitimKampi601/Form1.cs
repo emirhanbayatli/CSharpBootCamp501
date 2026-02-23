f1951971a9ed91d0053f2f5f0c71247480368588
Emirhan Bayatlı <emirhan.bayatli99@icloud.com>
2026-02-23 13:57:25 +0300

    lesson24 is completed

---
+++ CSharpEgitimKampi601/Form1.cs
@@ -0,0 +1,28 @@
+﻿using CSharpEgitimKampi601.Entities;
+using CSharpEgitimKampi601.Services;
+using System.Windows.Forms;
+
+namespace CSharpEgitimKampi601
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        CustomerOperations customerOperations = new CustomerOperations();
+        private void btnCustomerCreate_Click(object sender, System.EventArgs e)
+        {
+            var customer = new Customer()
+            {
+                CustomerName = txtCustomerName.Text,
+                CustomerSurname = txtCustomerSurname.Text,
+                CustomerBalance = decimal.Parse(txtCustomerBalance.Text),
+                CustomerCity = txtCustomerCity.Text,
+                CustomerShoppingCount = int.Parse(txtCustomerShoppingCount.Text),
+            };
+            customerOperations.AddCustomer(customer);
+            MessageBox.Show("Musteri Ekleme Islemi basarili", "Uyari", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
